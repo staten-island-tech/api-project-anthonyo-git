8bcba31ea71d45c7daf5e484f0dd18a228f49c24
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-12-08 09:24:32 -0500

    s

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -1,25 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../vite-project/counter.js'
+import '../css/style.css'
 
-const apiURL = 'https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=SjOQrhCncbcH7MpXTzSgct54f61IEfHv'
 
-async function getDATA(apiURL) {
-    let x = "xx"
-    try{
-        const response = await fetch(apiURL)
-        if(response.status !=200){
-            throw new Error(response.statusText);
-        }
-        const data = await response.json()
-        console.log(data)
-        document.querySelector("h2").textContent = timelines.intervals.values.temperature;
-    } catch (error) {
-    document.querySelector("h2").textContent = error;
-    }
+// key airplane bf7d9e2a73cd688369b66157defa7668
+async function getDATA() {
+    let res = await fetch(
+        "https://api.aviationstack.com/v1/flights?access_key=bf7d9e2a73cd688369b66157defa7668" 
+    );
+    let data = await res.json();
+    console.log(data)
 }
 
+getDATA()
 
 
 
