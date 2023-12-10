5d54d18a9d5088ef0e59095ee3e7a918f3203ea2
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-12-10 16:32:43 -0500

    flight api

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -1,17 +1,29 @@
 import '../css/style.css'
 
+// async function getDATA() {
+//     let res = await fetch(
+//         "https://api.aviationstack.com/v1/flights?access_key=bf7d9e2a73cd688369b66157defa7668" 
+//     );
+//     let data = await res.json();
+//     console.log(data)
+// }
 
-// key airplane bf7d9e2a73cd688369b66157defa7668
-async function getDATA() {
-    let res = await fetch(
-        "https://api.aviationstack.com/v1/flights?access_key=bf7d9e2a73cd688369b66157defa7668" 
-    );
-    let data = await res.json();
-    console.log(data)
+// getDATA()
+
+//c248667000mshe7328309b9d141dp119080jsn52ee67a4256a
+const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/NYC/ATL/20231211/';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c248667000mshe7328309b9d141dp119080jsn52ee67a4256a',
+		'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
 }
-
-getDATA()
-
-
-
-
