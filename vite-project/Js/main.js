8d1e46801c815304bb7dd1055e186b10ed4a95a6
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-12-11 09:24:37 -0500

    s

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -9,7 +9,7 @@ import '../css/style.css'
 // }
 
 // getDATA()
-
+//,yMsdSPnjW447bR weather api 23
 //c248667000mshe7328309b9d141dp119080jsn52ee67a4256a
 const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/NYC/ATL/20231211/';
 const options = {
@@ -27,3 +27,14 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+
+async function getDATA() {
+	let res = await fetch(
+		"http://api.weatherapi.com/v1/current.json?key=76c44a18082b4133979134522231112&q=atlanta georgia&aqi=yes" 
+	);
+let data = await res.json();
+	console.log(data)
+}
+
+getDATA()
