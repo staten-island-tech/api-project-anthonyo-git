15fda1dab1f26ac77fc819faed7e5e4ee05388e9
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-12-13 09:24:23 -0500

    s

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -9,25 +9,6 @@ import '../css/style.css'
 // }
 
 // getDATA()
-//,yMsdSPnjW447bR weather api 23
-//c248667000mshe7328309b9d141dp119080jsn52ee67a4256a
-const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/NYC/ATL/20231211/';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c248667000mshe7328309b9d141dp119080jsn52ee67a4256a',
-		'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
 
 async function getDATA() {
 	let res = await fetch(
